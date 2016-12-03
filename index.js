a009419fc8d92c5dc6afcadaf5ca3e70c2661bca
Philip Schwarz <pschwarz@expedia.com>
2016-12-03 15:25:20 +0000

    refactor

--- index.js
+++ index.js
@@ -12,9 +12,68 @@ var bodyParser = require('body-parser');
 
 const app = express();
 
-const articles = [{ title: 'Example' }];
 // instruct the app to use the `bodyParser()` middleware for all routes
 app.use(bodyParser());
+
+app.listen(3000);
+
+// Use Cases ////////////////////////////////////////////
+
+app.get("/messages", function (request, response) {
+  displayMessagesSentSoFar(response);
+});
+
+app.get('/message', function(request, response){
+  displayFormForSendingMessage(response);
+});
+
+app.post('/message', function(request, response){
+  var userName = request.body.userName;
+  var messageToSend = request.body.message;
+  sendMessageAndReportSuccess(userName, messageToSend, response);
+});
+
+// Use Case Handling ///////////////////////////////////
+
+function displayMessagesSentSoFar(response){
+  var query = 'SELECT * FROM message;';
+  var queryParameters = [];
+  var queryResultProcessor = function(queryResult){ response.send(queryResult.rows); };
+  runQuery(query, queryParameters, queryResultProcessor);
+};
+
+function  displayFormForSendingMessage(response) {
+  // The form's action is '/' and its method is 'POST',
+  // so the `app.post('/', ...` route will receive the
+  // result of our form
+  console.log('in here');
+  var html = '<form action="/message" method="post">' +
+    'Username:' +
+    '<input type="text" name="userName" placeholder="..." />' +
+    '<br>' +
+    'Message:' +
+    '<input type="text" name="message" placeholder="..." />' +
+    '<br>' +
+    '<button type="submit">Submit</button>' +
+    '</form>';
+
+  response.send(html);
+};
+
+function sendMessageAndReportSuccess(userName, messageToSend, response) {
+  var successReport = createReportTellingUserTheyHaveSentMessage(userName, messageToSend);
+  var query = 'INSERT INTO message (USERNAME, MESSAGE) VALUES ($1,$2);';
+  var queryParameters = [userName, messageToSend];
+  var queryResultProcessor = function (queryResult) { response.send(successReport); };
+  runQuery(query, queryParameters, queryResultProcessor);
+};
+
+function createReportTellingUserTheyHaveSentMessage(userName, message) {
+  return userName + ": you have just sent message '" + message + "'.<br>"
+    + '<a href="/message">Send another</a>';
+};
+
+// DB CONNECTION POOL ////////////////////////////////////////////////
 
 // create a config to configure both pooling behavior
 // and client options
@@ -30,31 +89,10 @@ var config = {
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
-
 //this initializes a connection pool
 //it will keep idle connections open for a 30 seconds
 //and set a limit of maximum 10 idle clients
 var pool = new pg.Pool(config);
-
-app.get("/messages", function (req, res) {
-  console.log("about to try connecting to DB");
-  // to run a query we can acquire a client from the pool,
-  // run a query on the client, and then return the client to the pool
-    pool.connect(function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-      client.query('SELECT * FROM message;', [], function(err, result) {
-        //call `done()` to release the client back to the pool
-        done();
-
-        if(err) {
-          return console.error('error running query', err);
-        }
-        res.send(result.rows);
-      });
-    });
-});
 
 pool.on('error', function (err, client) {
   // if an error is encountered by a client while it sits idle in the pool
@@ -64,50 +102,26 @@ pool.on('error', function (err, client) {
   // between your application and the database, the database restarts, etc.
   // and so you might want to handle it and at least log it out
   console.error('idle client error', err.message, err.stack)
-})
-
-app.get('/', function(req, res){
-  // The form's action is '/' and its method is 'POST',
-  // so the `app.post('/', ...` route will receive the
-  // result of our form
-  console.log('in here');
-  var html = '<form action="/" method="post">' +
-    'Username:' +
-    '<input type="text" name="userName" placeholder="..." />' +
-    '<br>' +
-    'Message:' +
-    '<input type="text" name="message" placeholder="..." />' +
-    '<br>' +
-    '<button type="submit">Submit</button>' +
-    '</form>';
-
-  res.send(html);
 });
 
-// curl -X POST http://localhost:3000/articles
-// This route receives the posted form.
-// As explained above, usage of 'body-parser' means
-// that `req.body` will be filled in with the form elements
-app.post('/', function(req, res){
-  var userName = req.body.userName;
-  var message = req.body.message;
+// DB QUERIES /////////////////////////////////////////////////
+
+function runQuery(query, parameters, processResult) {
+  console.log("about to try connecting to DB");
+  // to run a query we can acquire a client from the pool,
+  // run a query on the client, and then return the client to the pool
   pool.connect(function(err, client, done) {
     if(err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query('INSERT INTO message (USERNAME, MESSAGE) VALUES ($1,$2);', [userName, message], function(err, result) {
+    client.query(query, parameters, function(err, result) {
       //call `done()` to release the client back to the pool
       done();
 
       if(err) {
         return console.error('error running query', err);
       }
+      processResult(result);
     });
   });
-  var html =
-    userName + ": you have just sent message '" + message + "'.<br>" +
-    '<a href="/">Send another</a>';
-  res.send(html);
-});
-
-app.listen(3000);
+};
